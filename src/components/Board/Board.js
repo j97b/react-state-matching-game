775eb5a558bdc7ed6301de1a98445302472481de
James Barron <jamesbarron97@gmail.com>
2020-11-09 18:03:25 +0000

    module 2

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import './Board.css';
+import React from "react";
+import Tile from "../Tile/Tile";
+
+import "./Board.css";
 
 const Board = (props) => {
-
   const gridConfig = {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-  }
+  };
 
+  const mapTiles = props.tiles.map((tile) => {
+    <Tile {...tile} />;
+  });
 
   return (
-    <div className='Board' style={gridConfig}>
+    <div className="Board" style={gridConfig}>
+      {mapTiles}
     </div>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
